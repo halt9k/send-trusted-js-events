da0a59e7757d74c17f18263e445af14de8135722
halt9k <49090368+halt9k@users.noreply.github.com>
2022-11-28 01:00:08 +0300

    wip: hwnd and window logic separated

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from random import random
 
-from win_utility import *
-
 from win_enum import enum_processes, enum_process_windows
+from win_utility import *
+import datetime
+
+from winapi_utility import get_width, is_window_normal
 
 
 @dataclass
@@ -11,11 +13,13 @@ class TabInfo:
     side: int = 0
     last_text: str = ''
     total_clicks: int = 0
+    last_click_ms: datetime.datetime = datetime.datetime.now()
     # last_click: (int, int) = (0, 0)
     closed: bool = False
 
 
 browser_tabs = {}
+new_window = False
 
 
 def del_closed_tabs():
@@ -30,24 +34,28 @@ def del_closed_tabs():
         browser_tabs.pop(key)
 
 
-def test_window_setup(hwnd):
+def is_popup(hwnd):
+    # tricvky: not updated on tabs, must be checked again after activation
     wh = get_width(hwnd)
-    return wh in range(40, 400) and is_window_normal(hwnd)
+    return is_window_normal(hwnd) and (wh in range(100, 450))
 
 
-def process_window(hwnd, text):
-    if not test_window_setup(hwnd):
-        return
+def process_window(hwnd, text, rearrange):
+    if not is_popup(hwnd):
+        return rearrange
 
     if hwnd not in browser_tabs.keys():
         browser_tabs[hwnd] = TabInfo()
+        rearrange = True
     browser_tabs[hwnd].closed = False
 
-    n = list(browser_tabs.keys()).index(hwnd)
-    setup_window(hwnd, n)
+    init_failed = browser_tabs[hwnd].total_clicks == 0 and not rearrange
+    if rearrange or init_failed:
+        n = list(browser_tabs.keys()).index(hwnd)
+        setup_window(hwnd, n)
 
     if browser_tabs[hwnd].last_text == text:
-        return
+        return rearrange
 
     browser_tabs[hwnd].total_clicks += 1
     # if sqrt(sites[hwnd].total) / 20 > random():
@@ -55,11 +63,14 @@ def process_window(hwnd, text):
     # continue
 
     browser_tabs[hwnd].last_text = text
-    process_click(hwnd, text)
+    if process_click(hwnd, text):
+        browser_tabs[hwnd].last_click_ms = datetime.datetime.now()
+
+    return rearrange
 
 
 def click_check():
-    global browser_tabs
+    global browser_tabs, new_window
     del_closed_tabs()
 
     procs = enum_processes(process_name="firefox.exe")
@@ -72,8 +83,12 @@ def click_check():
         # print(proc_text)
         for hwnd, text in data:
             # hwnd may became obsolete on any internal step
+            rearrange = new_window
+            new_window = False
             try:
-                process_window(hwnd, text)
+                rearrange = process_window(hwnd, text, rearrange)
+                if rearrange:
+                    new_window = True
             except Exception as e:
                 print(e)
 

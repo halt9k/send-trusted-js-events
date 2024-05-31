3f6bb478e06fe30c9826a64b6ad94182a5411eb5
halt9k <49090368+halt9k@users.noreply.github.com>
2024-05-31 15:46:10 +0300

    upd

--- src/Test.user.js
+++ src/Test.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @match      *://*/*
+// @match        https://www.google.com/
 // @grant unsafeWindow
 // @grant window.close
 // @grant window.focus
@@ -18,9 +18,9 @@
 // require http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // require https://gist.github.com/raw/2625891/waitForKeyElements.js
 
-const URL = 'https://www.google.com/'
 const CSS_SEL_GOOGLE_SEARCH = "div.RNNXgb"
 const CSS_SEL_GOOGLE_SEARCH_BTN = ".FPdoLc"
+const CSS_SEL_GOOGLE_LOGO = ".lnXdpd"
 
 const REQ_CLICK = 'REQ CLICK ${X} ${Y}'
 const REQ_KEYS = 'REQ KEYS ${K}'
@@ -56,84 +56,94 @@ function getClientPosPx(elem, posAtMiddle){
 }
 
 
+function unsafeQuerySelector(selector)
+	{
+	let elem = document.querySelector(selector)
+	if (!elem)
+		throw new Error(`Selector {selector} not found, possibly outdated`);
+	return elem;
+	}
+
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
-async function waitUntilDone() {
+async function waitUntil(ifDoneFunc) {
 	let delay = 200
 	for (let i = 0; i < 5000; i++)
 		{
 		await sleep(200)
-        if (document.title == DONE)
+        if (ifDoneFunc())
             return;
 		if (i % 50 == 0)
-			log_ex(`Waiting for trusted command ${i * delay}ms`, location);
+			log_ex(`Waiting for winapi command fired extrernally ${i * delay}ms`, location);
 		}
 }
 
 
-async function trustedClick(elem)
+function inFocusedTree(elem) {
+	debugger;
+	return document.activeElement
+}
+
+
+function requestTrustedClick(elem)
 	{
 	let xy = getClientPosPx(elem, true)
 	document.title = REQ_CLICK.replace('${X}', xy.x).replace('${Y}', xy.y);
-    await waitUntilDone();
 	}
 
 
-async function trustedKeys(elem, keys)
+function requestTrustedKeys(elem, keys)
 	{
 	elem.focus()
 	document.title = REQ_KEYS.replace('${K}', keys);
-    await waitUntilDone();
+	}
+
+
+async function trustedSelect(selector)
+	{
+	let xy = getClientPosPx(elem, true)
+	document.title = REQ_CLICK.replace('${X}', xy.x).replace('${Y}', xy.y);
 	}
 
 
 async function main()
 	{
-	if (document.location.href != URL) {
-		log_ex('Correct URL not detected, skipping main', location)
-		return;
-	}
-	// await sleep(400)
-	// if (document.title != NEW_WINDOW_TITLE) {
-	//	log_ex('Title not changed, try run script via context menu', location)
-	//	return;
-	//} else {
-	//	log_ex('run_loop', location)
-	//}
+	// Not required since google auto focuses on the search,
+	// but focus switch just to ensure clciks are working correctly
+	let logo = unsafeQuerySelector(CSS_SEL_GOOGLE_LOGO);
+	requestTrustedClick(logo);
+	await waitUntil(inFocusedTree(logo));
 
-    let search = document.querySelector(CSS_SEL_GOOGLE_SEARCH)
-	await trustedClick(search)
-    await trustedKeys(search, 'H e l l o W o r l d')
+	let search = unsafeQuerySelector(CSS_SEL_GOOGLE_SEARCH);
+	requestTrustedClick(search);
+	await waitUntil(inFocusedTree(search));
+
+    await requestTrustedKeys(search, 'H e l l o W o r l d')
+	await waitUntil(() => search.text == 'HelloWorld');
+
     let btn = document.querySelector(CSS_SEL_GOOGLE_SEARCH_BTN)
-    await trustedClick(btn)
+	let prevTitle = document.title
+    await waitUntil(() => document.title != prevTitle);
 	}
 
 
-async function open_new_tab_workaround(){
-	if (document.location.href == URL)
-		return;
-
-	log_ex('New tab is yet desired because WinApi fails to set window caption to DONE on usual tab,\n' +
-		   'but it works on tab opened with UserScript.\n' +
-	       'Skipping DONE and relying on passive checks is an option without new window.\n' +
-	       'Switching to network comm instead of caption is also a suggestion', location)
-
+function open_new_tab(){
+	// Unused. May be handy for background automation, since external clicks are possible to unfocused window
 	log_ex('open_new_tab', location)
 	let w = window.outerWidth;
 	let h = window.outerWidth;
 	let windowFeatures = `width=${w},height=${h},left=0,top=0`
 
-	// currentURL = window.location.href
-	let wnd = window.open(URL, "_blank", windowFeatures);
+	let currentURL = window.location.href
+	let wnd = window.open(currentURL, "_blank", windowFeatures);
 	if (!wnd) {
 		log_ex('Cannot reference a new window, possibly popup was blocked.', location)
 		return;
 	}
 }
 
-
-open_new_tab_workaround()
 main()
